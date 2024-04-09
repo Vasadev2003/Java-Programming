ac3d4dbac0d5ecaa28dea81e23de3939c97849eb
VasaDev2003 <vasadev2023@gmail.com>
2024-04-09 11:20:18 +0530

    Variables And Data Types

---
+++ Hello.java
@@ -0,0 +1,19 @@
+ class Hello 
+ {
+    public static void main(String a[]) 
+    {
+        int num1 = 10;
+        int num2 = 20;
+        int ans = num1+num2;
+
+        System.out.print("Sum of is "+ans);
+
+        // Literals this some thing fishy that the charater is treated as an integer and we can perform actions on it 
+
+        char name = 'v';
+        System.out.println(++name);
+    }
+}
+
+
+
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Java Programming With Telusko
 
 1.Setup the Environment
+2.started with variables and its behaviour
+
+# Variables and Data Types
+
+1.The thing that suprised me is the string that can be treated as an integer
+
