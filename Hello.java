f843b756fe43aa5a0aa13a60d31a323254598ad4
VasaDev2003 <vasadev2023@gmail.com>
2024-04-09 19:03:23 +0530

    operators

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
  {
     public static void main(String a[]) 
     {
-        int num1 = 10;
+        /* int num1 = 10;
         int num2 = 20;
         int ans = num1+num2;
 
@@ -12,6 +12,23 @@
 
         char name = 'v';
         System.out.println(++name);
+
+        //Type conversion and Casting
+
+        int num3 = 50;
+        float num = 12.5f;
+        int change = (int)num;
+        float change2 = (float)num3;
+        System.out.println(change);
+        System.out.println(change2); */
+
+        // pre increment and post increment 
+
+        int num1 = 8;
+      /*   int post = num1++; */
+        int pre = num1++;
+       /*  System.out.println(post); */
+        System.out.println(pre);
     }
 }
 
